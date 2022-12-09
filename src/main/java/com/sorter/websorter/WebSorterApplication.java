5c4eb7b3f384f3e8dd339172d8982ab27827ebc9
Jeffrey O. Adams <frexjeff@gmail.com>
2022-12-09 23:08:06 +0000

    Update on test data and readfile

--- src/main/java/com/sorter/websorter/WebSorterApplication.java
+++ src/main/java/com/sorter/websorter/WebSorterApplication.java
@@ -3,6 +3,7 @@ package com.sorter.websorter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -11,6 +12,9 @@ public class WebSorterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebSorterApplication.class, args);
+
+        File file = new File("src/test/resources/testdata");
+        System.out.println(file.getAbsoluteFile());
     }
 
 }
--- src/test/java/com/sorter/websorter/WebSorterApplicationTests.java
+++ src/test/java/com/sorter/websorter/WebSorterApplicationTests.java
@@ -1,13 +1,26 @@
 package com.sorter.websorter;
 
+import com.sorter.websorter.services.DataReader;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class WebSorterApplicationTests {
 
+	@Autowired
+	DataReader<Integer> dataReader;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void FileReadTest(){
+		Integer[] data = dataReader.readFile();
+		//System.out.println(Arrays.toString(data));
+		assertThat(data).isNotEmpty();
 	}
 
 }
